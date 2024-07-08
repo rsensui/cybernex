2a17a76d7777af31daafa1c785c1a68b3ee961c6
rsensui <ryosuke.sensui@cybernex.co.jp>
2024-07-08 12:15:58 +0900

    Set up GitHub Actions for gh-pages deployment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <Router basename="/cybernex">
     <App />
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
